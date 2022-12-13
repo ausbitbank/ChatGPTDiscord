4a338bef879751ff175b96330176757d189480f5
ausbitbank <ausbitbank@users.noreply.github.com>
2022-12-13 10:39:29 +1000

    Update ChatGPTdiscord.py

--- ChatGPTdiscord.py
+++ ChatGPTdiscord.py
@@ -1,5 +1,8 @@
 import requests, json, discord, logging, sys, signal, asyncio, functools, typing
 from revChatGPT.revChatGPT import Chatbot
+#from PIL import Image
+#from io import BytesIO
+#from pytesseract import image_to_string
 
 '''
 # API access on 127.0.0.1:9879/chatgpt?prompt=PROMPT
@@ -21,24 +24,12 @@ def promptapi():
 def run_api():
     api.run(host='localhost', port=9879)
 
-#Uploaded image recognition via inceptionV3
-import tensorflow as tf
-model = tf.keras.applications.InceptionV3(include_top=True, weights='imagenet')# Load the Inception-V3 model
-
-def image_url_to_description(url):  # Define a function that takes a URL as an argument
-    image = tf.keras.preprocessing.image.load_img(url, target_size=(299,299))  # Load the image from the URL and resize it to (299,299)
-    image = tf.keras.preprocessing.image.img_to_array(image)  # Convert the image to a NumPy array
-    image = tf.keras.applications.inception_v3.preprocess_input(image)  # Pre-process the image for Inception v3
-    image = tf.expand_dims(image, 0)  # Add a batch dimension to the image
-    predictions = model.predict(image)  # Use the Inception v3 model to make predictions on the image
-    decoded_predictions = tf.keras.applications.inception_v3.decode_predictions(predictions, top=5)  # Decode the predictions to make them human-readable
-    print("Image description:")  # Print a message indicating the beginning of the image description
-    image_desc = "We detect the following elements in the image using inception v3 from Google\nLabel : Probability\n"  # Define a string containing a message about the predictions
-    for prediction in decoded_predictions[0]:  # Loop through the predictions
-        print(f" - {prediction[1]}: {prediction[2]:.2f}")  # Print each prediction
-        image_desc += f"{prediction[1]} : {prediction[2]}\n"  # Add the prediction to the description string
-    return image_desc  # Return the description of the image
 '''
+#def extract_text_from_image_url(image_url):
+#    response = requests.get(image_url)
+#    image = Image.open(BytesIO(response.content))
+#    return image_to_string(image)
+
 def split_string_into_chunks(string, chunk_size):
   chunks = []# Create an empty list to store the chunks
   while len(string) > 0:# Use a while loop to iterate over the string
@@ -47,23 +38,23 @@ def split_string_into_chunks(string, chunk_size):
     string = string[chunk_size:]# Remove the chunk from the original string
   return chunks# Return the list of chunks
 
-'''
-from bs4 import BeautifulSoup
-def get_google_response(query):
-    url='https://www.google.com/search?q='+query
-    response=requests.get(url)
-    soup=BeautifulSoup(response.text, "html.parser")
-    results=soup.find_all('div', class_='g')
-    print(results)
-    result_str='Top 5 google results for: '+query
-    for result in results[:5]:
-        title=result.find('h3')
-        link=result.find('a')
-        description=result.find('span',class_='st')
-        result_str+=f"{title.text}\n{link['href']}\n{description.text}"
-    print(result_str)
-    return result_str
-'''
+def tidy_response(i):# Optionally spoilerify or hide the most repetitive annoying nothing responses, rebrand to EvilCorp
+    spoiler_bad_responses=False
+    hide_bad_responses=True
+    rebrand_responses=True
+    bad_responses=["As a large language model trained by OpenAI,","As a language model trained by OpenAI,","My training data has a cutoff date of 2021, so I don't have knowledge of any events or developments that have occurred since then.","I'm not able to browse the internet or access any new information, so I can only provide answers based on the data that I was trained on.","I don't have the ability to provide personal opinions or subjective judgments, as I'm only able to provide objective and factual information.","I'm not able to engage in speculative or hypothetical discussions, as I can only provide information that is based on verifiable facts.","I'm not able to provide medical, legal, or financial advice, as I'm not a qualified professional in these fields.","I'm not able to engage in conversations that promote or encourage harmful or offensive behavior.","I don't have personal experiences or opinions, and I can't provide personalized advice or recommendations.","As a language model, I'm not able to perform actions or execute commands. I can only generate text based on the input I receive.","I'm not able to provide direct answers to questions that require me to make judgments or evaluations, such as questions that ask for my opinion or perspective on a topic.","I can provide information on a wide range of subjects, but my knowledge is limited to what I have been trained on and I do not have the ability to browse the internet to find new information","I do not have the ability to browse the internet or access information outside of what I have been trained on."]
+    if i.find("`") == -1: # Only attempt if no code block is inside the response
+        if spoiler_bad_responses:
+            #bad_responses_found=[response.replace(response, "||" + response + "||") for response in bad_responses if response in i]
+            bad_responses_found=[response for response in bad_responses if response in i]
+            bad_responses_string = "".join(bad_responses_found)
+        if hide_bad_responses:
+            for br in bad_responses:i=i.replace(br, "")
+        if spoiler_bad_responses and bad_responses_string != '':i+='\n||'+bad_responses_string+'||'
+    if rebrand_responses:
+        i=i.replace("OpenAI", "EvilCorp")
+        i=i.replace("!Dream:", "!dream ")
+    return i
 
 def to_thread(func: typing.Callable) -> typing.Coroutine:
     @functools.wraps(func)
@@ -81,9 +72,11 @@ def get_answer(chatbot,query):
 if __name__ == "__main__":
 #    thread = Thread(target=run_api);thread.start()
     with open("config.json", "r") as f: config = json.load(f)
-    chatbot = Chatbot(config, conversation_id=None);chatbot.refresh_session()
     intents = discord.Intents.default();intents.message_content = True
     client = discord.Client(intents=intents)
+    chatbot = Chatbot(config, conversation_id=None)
+    userdb={}
+
     @client.event
     async def on_ready():
         print(f'We have logged in as {client.user}')
@@ -91,51 +84,63 @@ if __name__ == "__main__":
     @client.event
     async def on_message(message):
         if message.author == client.user: return
-        if message.channel.id != config["discord_channel"]: return
+        if message.channel.id != config["discord_channel"] and type(message.channel)!=discord.DMChannel: return
         if message.author.bot: return
-        if message.content == 'refresh' and message.author.id == config['discord_admin_id']: chatbot.refresh_session();return
-        if message.content == 'restart' and message.author.id == config['discord_admin_id']: exec(open(sys.argv[0]).read());return
+        if message.content == 'refresh' and message.author.id == config['discord_admin_id']: chatbot.refresh_session(); await message.add_reaction("🔄"); print("refresh session"); return
+        if message.content == 'restart' and message.author.id == config['discord_admin_id']: os.execl(__file__, *sys.argv);return
+        if message.content == 'reset' and message.author.id == config['discord_admin_id']: chatbot.reset_chat;await message.add_reaction("💪"); print("reset chat"); return
         if message.content.startswith('!dream'):return
-        '''
-        if message.content.startswith('google'):
-            chunks=split_string_into_chunks(get_google_response(message.content[7:]),1950)
-            for chunk in chunks: await message.reply(chunk)
-            return
-        if message.attachments and message.attachments[0].width and message.attachments[0].height:
-            image_url = message.attachments[0].proxy_url
-            image_desc = image_url_to_description(image_url)
-            await message.reply(image_desc)
-            return
-        '''
+        if message.content.startswith('!wiki'):
+            url='https://en.wikipedia.org/wiki/'+message    .content[6:]
+            print(url)
+            wikiresponse=await wiki_to_text(url)
+            print(wikiresponse)
+            await message.reply(wikiresponse)
         longquery=''
+        if message.attachments and message.attachments[0].width and message.attachments[0].height:
+            #image_url = message.attachments[0].proxy_url
+            #image_desc = extract_text_from_image_url(image_url)
+            #longquery=await message.reply(image_desc)
+            return
         if message.attachments and message.attachments[0].content_type.startswith('text'):
             print('text attachment found, adding to prompt')
             attachment=message.attachments[0]
             data=await attachment.read()
             longquery=data.decode()
-
         if message.mentions:
             for user in message.mentions:
                 if user != client.user: return
-        print(message.content)
+        print(message.author.name+':'+message.content)
         try:
             query=message.content
-            await message.add_reaction("👀")
             if longquery and longquery != '':
                 query=message.content+'\n```'+longquery+'\n```'
-            response=await get_answer(chatbot,query)
+            cid=None
+            did=message.channel.id
+            cb=chatbot
+            if did in userdb:
+                cid=userdb[message.channel.id]
+            else:
+                cid=None
+            #cb.conversation_id=cid
+            #if type(message.channel)==discord.DMChannel:#DM
+            #else:#In channel
+            response=await get_answer(cb,query)
+            userdb[did]={'cid':response['conversation_id']}
+            print(userdb)
             print(response)
-            chunks=split_string_into_chunks(response['message'],1950)
+            print('ai:'+response['message'])
+            r=tidy_response(response['message'])
+            chunks=split_string_into_chunks(r,1975) # Make sure response chunks fit inside a discord message (max 2k characters)
             for chunk in chunks:
-                chunk=chunk.replace("As a large language model trained by OpenAI,", "")
-                chunk=chunk.replace("As a language model trained by OpenAI, ", "")
-                chunk=chunk.replace("OpenAI", "EvilCorp")
-                chunk=chunk.replace("!Dream:", "!dream ")
                 await message.reply(chunk)
-                await message.add_reaction("☑️")
 
         except Exception as e:
             print("Something went wrong!")
             print(e)
+            if e.startswith("Expecting value:"):
+                print('restarting session')
+                await message.reply('Connection problem found, restarting, re-ask your question in a moment')
+                os.execl(__file__, *sys.argv)
             await message.add_reaction("💩")
     client.run(config["discord_bot_token"])
